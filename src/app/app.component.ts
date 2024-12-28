260b4a95454663734d70cda28bf1150f6b84314b
Camdyn Hamilton <c02hamil@gmail.com>
2024-12-27 22:27:03 -0500

    feat: general layout of topic selector/project generator section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { TitleCardComponent } from './title-card/title-card.component';
+import { TopicSelectorComponent } from './topic-selector/topic-selector.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, TitleCardComponent],
+  imports: [HeaderComponent, TitleCardComponent, TopicSelectorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/topic-selector/topic-selector.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topic-selector',
+  imports: [],
+  templateUrl: './topic-selector.component.html',
+  styleUrl: './topic-selector.component.scss'
+})
+export class TopicSelectorComponent {
+
+}
