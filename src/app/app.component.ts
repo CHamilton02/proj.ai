7a087426e95673bb88368d9e46fe8c0ee01a5051
Camdyn Hamilton <c02hamil@gmail.com>
2025-01-01 15:47:27 -0500

    create project information section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,18 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { TitleCardComponent } from './title-card/title-card.component';
 import { TopicSelectorComponent } from './topic-selector/topic-selector.component';
+import { ProjectInformationComponent } from './project-information/project-information.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, TitleCardComponent, TopicSelectorComponent],
+  imports: [
+    HeaderComponent,
+    TitleCardComponent,
+    TopicSelectorComponent,
+    ProjectInformationComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/project-information/project-information.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-information',
+  imports: [],
+  templateUrl: './project-information.component.html',
+  styleUrl: './project-information.component.scss'
+})
+export class ProjectInformationComponent {
+
+}
