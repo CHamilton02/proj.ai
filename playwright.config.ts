27f1f899751a11d680c75e4f34f079c709a0b3a9
Camdyn Hamilton <c02hamil@gmail.com>
2025-02-12 18:08:20 -0500

    fix: update Playwright script

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
+    command: 'npm run start:dev',
     url: 'http://localhost:4200',
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
