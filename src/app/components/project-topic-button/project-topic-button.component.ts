0e400cdc9d1ae74cccbc8c66e846d858e1dba5ce
Camdyn Hamilton <c02hamil@gmail.com>
2025-02-23 13:46:28 -0500

    feat: improve mobile version of project topic buttons

--- src/app/components/project-topic-button/project-topic-button.component.ts
+++ src/app/components/project-topic-button/project-topic-button.component.ts
@@ -12,4 +12,23 @@ export class ProjectTopicButtonComponent {
   projectTopic = input.required<string>();
   suggestedTopic = input<boolean>();
   projectService: ProjectService = inject(ProjectService);
+  confirmUserTap = false;
+
+  toggleProjectButton() {
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|mobile|CriOS/i.test(
+        navigator.userAgent
+      ) &&
+      !this.confirmUserTap
+    ) {
+      this.confirmUserTap = true;
+      document
+        .getElementById(`project-topic-button-${this.projectTopic()}`)
+        ?.focus();
+    } else if (this.suggestedTopic()) {
+      this.projectService.addProjectTopic(this.projectTopic());
+    } else {
+      this.projectService.removeProjectTopic(this.projectTopic());
+    }
+  }
 }
